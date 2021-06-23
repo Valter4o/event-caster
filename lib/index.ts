60885aa3245b2a35e30f5054bf98874218fcf133
Valentin Tashkov <valentin.tashkov@gmail.com>
2021-06-23 16:25:06 +0300

    Innitial commit

---
+++ lib/index.ts
@@ -0,0 +1,113 @@
+interface Caster {
+  events: {};
+  options: any;
+  cast: (eventName: string, params?: any) => void;
+  on: (eventName: string, cb: Function, options?: any) => void;
+  once: (eventName: string, cb: Function) => void;
+  off: (eventName: string) => void;
+  offAll: () => void;
+  removeListener: (eventName: string) => void;
+  removeAllListeners: () => void;
+  eventNames: () => string[];
+  clearSubs: (eventName: string) => void;
+}
+
+function sanitizeParams(params: any) {
+  const sanitizedData: any = {};
+  const paramKeys = Object.keys(params);
+  for (let ind = 0; ind < paramKeys.length; ind++) {
+    const param: string = paramKeys[ind];
+    const isParamPrivate: boolean = param[0] === "_";
+    if (!isParamPrivate) {
+      sanitizedData[param] = params[param];
+    }
+  }
+  return sanitizedData
+}
+
+function cast(eventName: string, params?: any): void {
+  if (this.events.hasOwnProperty(eventName)) {
+    const subs = this.events[eventName].subs;
+    for (let i = 0; i < subs.length; i++) {
+      const sub = subs[i];
+      const sanitizedParams = !!params ? sanitizeParams(params) : {};
+      sub.cb(sanitizedParams);
+      if (sub.once) {
+        this.events[eventName].subs = [
+          ...subs.slice(0, 1),
+          ...subs.slice(i + 1),
+        ];
+      }
+    }
+  } else {
+    if (!params._safeCast) {
+      throw new Error("Emitter error: There is no such event registered");
+    }
+  }
+}
+
+function on(eventName: string, cb: Function, options = {}): void {
+  if (this.hasOwnProperty(eventName)) {
+    this.events[eventName].subs.push({ cb, ...options });
+  } else {
+    this.events[eventName] = {
+      subs: [{ cb: cb, options }],
+    };
+  }
+}
+
+function once(eventName: string, cb: Function) {
+  if (this.hasOwnProperty(eventName)) {
+    this.events[eventName].subs.push({ cb, once: true });
+  } else {
+    this.events[eventName] = {
+      subs: [{ cb: cb, once: true }],
+    };
+  }
+}
+
+function eventNames(): string[] {
+  return Object.keys(this.events);
+}
+
+function off(eventName: string) {
+  if (this.events.hasOwnProperty(eventName)) {
+    delete this.events[eventName];
+  }
+}
+
+function offAll() {
+  const events = Object.keys(this.events);
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    delete this.events[event];
+  }
+}
+
+function removeListener(eventName: string) {
+  this.off(eventName);
+}
+
+function removeAllListeners() {
+  this.offAll();
+}
+
+function clearSubs(eventName: string) {
+  this.events[eventName].length = 0;
+}
+
+const caster: Caster = {
+  events: {},
+  options: {},
+  cast,
+  on,
+  once,
+  off,
+  offAll,
+  removeListener,
+  removeAllListeners,
+  eventNames,
+  clearSubs,
+};
+
+export default caster as Caster;
